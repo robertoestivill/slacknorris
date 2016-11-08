945f22f551adc92cbaa6682e8d39778e23252688
Roberto Estivill <robertoestivill@gmail.com>
2016-11-08 23:31:32 +0100

    Adding https redirect

--- src/main/java/slacknorris/config/Redirect.java
+++ src/main/java/slacknorris/config/Redirect.java
@@ -2,7 +2,7 @@ package slacknorris.config;
 
 public final class Redirect {
 
-  public static final String HOME = "http://robertoestivill.com/slacknorris";
+  public static final String HOME = "https://robertoestivill.com/slacknorris";
   public static final String ERROR = HOME + "/error.html";
   public static final String SUCCESS = HOME + "/success.html";
   public static final String CONTACTED = HOME + "/contacted.html";
